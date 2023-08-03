4dca813d1aa3fca69c303f967ee8609efcce17a8
ermac_ <ermac_@mail.ru>
2023-08-03 21:32:15 +0300

    Добавлена задача 8

---
+++ seminar_1/exercise_8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Объявление переменных
+string str_number;
+int number;
+
+//Ввод числа
+Console.WriteLine("Введите число");
+str_number = Console.ReadLine();
+number = int.Parse(str_number);
+Console.WriteLine();
+
+//Вычисление и вывод результата
+for (int i = 2; i <= number; i = i + 2)
+{
+    Console.WriteLine(i);
+}
+
